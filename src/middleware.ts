538de850a2538e0a7cd0edf6d9e2b97715296837
ElielsonU <elielson7756@gmail.com>
2023-04-04 11:08:11 -0300

    middleware changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,12 @@ import { NextMiddleware, NextResponse } from "next/server";
 export const middleware: NextMiddleware = (req, res) => {
     const loggedAS = req.cookies.get("loggedAs")
     const href = req.nextUrl.href
-    
+    const dangerousHref = (href.includes("_next") || href.includes("."))
     if (loggedAS) {
-        if (href != "http://localhost:3000/home" && !href.includes("_next")) {
+        if (href != "http://localhost:3000/home" && !dangerousHref) {
             return NextResponse.redirect("http://localhost:3000/home")
         }
-    } else if (href != "http://localhost:3000/" && !href.includes("_next")) {
+    } else if (href != "http://localhost:3000/" && !dangerousHref) {
         return NextResponse.redirect("http://localhost:3000/")
     }
 }
